8763219d343f111a19fe7b2a686dd5917184fec1
Michael Cole <dutymanagers-secure@outlook.com>
2026-02-02 12:53:06 -0500

    first version

---
+++ main.py
@@ -0,0 +1,18 @@
+contacts =[]
+
+def add_contact(name, phone):
+    contacts.append({"name": name, "phone": phone})
+    print(f"Contact {name} added successfully!")
+
+def view_contacts():
+  if not contact:
+  print("No contacts found.")
+  else:
+     print("\nContact List:")
+     for i, contact in enumerate(contacts, 1):
+     print(f"{i}. {contact['name']} - {contact['phone']}")
+
+add_contact("Alice", 1234567890")
+add_contact("Bob", 9876543210")
+
+view_contacts()
